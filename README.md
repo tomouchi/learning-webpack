3afecce8e5a9e5856805cbcb746236ba3aaad43e
tomouchi <wisteria-0004@m3.gyao.ne.jp>
2019-05-28 08:11:18 +0900

    add html-webpack-plugin

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Learning webpack
 ## MEMO
-## 手順
-1.  `npm init -y`  
+- `npm init -y`  
     - `-y`オプションを付けると既定値でpackage.jsonを作成する。
     - `-y`オプションを付けないといろいろ設定を訊かれる。
+- `npm i --save-dev html-webpack-plugin`
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: `${__dirname}/dist`,
     filename: "main.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "learning webpack",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
   devServer: {
     contentBase: "./dist",
   },
