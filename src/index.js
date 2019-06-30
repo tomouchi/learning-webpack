882eac2a68304a7d34dbb237ad8231c54bbf3041
tomouchi <29200586+tomouchi@users.noreply.github.com>
2019-06-30 22:43:58 +0900

    add comment

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Vue from "vue";
 import "./style.file-loader.css";
 import "./style.style-loader.css";
 import "./style.css";
-import("react");
-import("moment");
+import("react"); //splitChunksの確認で使用 同じbundleファイルにbundleされる
+import("moment"); //splitChunksの確認で使用 動的・静的で異なるファイルにbundleされる
 
 jQuery(".jquery").text("added text"); //Shimming
 jQuery(".exportText").text(text); //Shimming
--- src/sub.js
+++ src/sub.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueSub from "./vue/sub";
-import("react");
-import moment from "moment"
+import("react"); //splitChunksの確認で使用 同じbundleファイルにbundleされる
+import moment from "moment" //splitChunksの確認で使用 動的・静的で異なるファイルにbundleされる
 
 new Vue({
   render: h => h(VueSub)
